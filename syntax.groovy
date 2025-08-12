ec52b5ad16ca6db12d4f091f52649df2fda3c9c7
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-08-12 15:28:45 +0530

    Update syntax.groovy

--- syntax.groovy
+++ syntax.groovy
@@ -17,3 +17,55 @@
                 echo "this is code deploy stage"
                 }
              }
+
+               // printenv pipeline
+pipeline {
+    agent any 
+    stages {
+        stage ('jayant'){
+            steps {
+                sh 'printenv'
+            }
+        }
+    }
+}
+
+// Default variable pipeline
+pipeline {
+    agent any 
+    stages {
+        stage ('jayant'){
+            steps {
+                echo "Jenkins job Build id is $BUILD_ID"
+            }
+        }
+        stage ('mayur'){
+            steps {
+                echo "Jenkins home directory is $HUDSON_HOME"
+            }
+        }
+        
+    }
+}
+
+// environment variables pipeline
+
+pipeline {
+    agent any 
+    environment {
+        Prod_Ip = "192.168.9.0"
+        DEV_Ip = "192.168.8.0"
+    }
+    stages {
+        stage ('env-variable'){
+            steps {
+                echo "Environment variable is $Prod_Ip"
+            }
+        }
+        stage ('env-DEVIP'){
+            steps {
+                echo "Environment variable is $DEV_Ip"
+            }
+        }
+    }
+}
